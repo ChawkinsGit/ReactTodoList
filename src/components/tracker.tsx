14f17fc7201aa6cd0645a9946914efb8e91e0d2e
ChawkinsGit <cmhawkins914@gmail.com>
2025-01-31 23:46:47 -0600

    started todo list, only was able to create list and array, other errors persisted

---
+++ src/components/tracker.tsx
@@ -0,0 +1,18 @@
+
+
+function TrackerGroup() {
+let animes = ['Attack On Titan', 'Black Clover', 'One Piece']
+  return (
+  <>
+    <div>
+      <ol className="list-group list-group-numbered">
+    <li className="list-group-item">A list item</li>
+    <li className="list-group-item">A list item</li>
+    <li className="list-group-item">A list item</li>
+  </ol>
+  </div>;
+  </>
+  )
+};
+
+export default TrackerGroup;
